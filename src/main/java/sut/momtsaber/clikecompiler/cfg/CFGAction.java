14bff48a0af0f2c4f6afd17f017b47ee61d2062e
momt99 <mohammad.omt99@yahoo.com>
2019-06-12 19:17:41 +0430

    Fix bug in grammar Add some actions names

--- src/main/java/sut/momtsaber/clikecompiler/cfg/CFGAction.java
+++ src/main/java/sut/momtsaber/clikecompiler/cfg/CFGAction.java
@@ -3,4 +3,34 @@ package sut.momtsaber.clikecompiler.cfg;
 public class CFGAction extends CFGSymbol
 {
     private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public static class Names
+    {
+        public static final String PUSH_TOKEN = "pt";
+
+        public static final String START_PROGRAM = "pstart";
+        public static final String END_PROGRAM = "pend";
+
+        public static final String BEGIN_SCOPE = "begin_scope";
+        public static final String END_SCOPE = "end_scope";
+
+        public static final String DECLARE_VAR = "var_dec";
+        public static final String DECLARE_ARRAY = "arr_dec";
+
+        public static final String DECLARE_FUNC = "func_dec";
+        public static final String DECLARE_PARAM_VAR = "param_var_dec";
+        public static final String DECLARE_PARAM_ARRAY = "param_arr_dec";
+        public static final String FUNC_INIT = "func_init";
+        public static final String PUSH_RETURN_VAL = "push_retval";
+        public static final String PUSH_VOID = "push_void";
+        public static final String RETURN = "return";
+
+        public static final String CONTINUE = "continue";
+        public static final String BREAK = "break";
+    }
 }
